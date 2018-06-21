af440bd1db427b7ce4fe3cfecd730f8adf584793
Shubham Sharma <shubhamsharma5922@gmail.com>
2018-06-22 00:10:06 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+
+var app = express()
+
+hbs.registerPartials(__dirname+'/views/partials/')
+hbs.registerHelper('currentYear',()=>{return new Date().getFullYear()})
+hbs.registerHelper('screamIt',(text)=>{ return text.toUpperCase()})
+
+app.use(express.static(__dirname+'/public_static'))
+
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log',log + '\n',(e)=>{
+        if(e) console.log(e);
+    })
+    next()
+})
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance',{pageTitle:'maintenance'})
+// })
+app.get('/',(req,res)=>{
+    //res.send('<h1>hello world!</h1>')
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        text:'welcome'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    "use strict";
+    res.render('about.hbs',{
+        pageTitle : 'About us',
+    })
+})
+app.get('/bad',(req,res)=>{
+    "use strict";
+    res.send('error messsage')
+})
+
+app.listen(3000,()=>console.log('server started'))
